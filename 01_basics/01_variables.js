36750d28e7cc2f1c5c8c240fbc92e696f2eededa
Deepa-Balan <139548667+Deepa-Balan@users.noreply.github.com>
2024-06-19 10:14:06 +0000

    variables and constants

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "somename@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 not allowed
+accountEmail = "somebody@gmail.com"
+accountPassword = "21212121"
+accountCity = "Benguluru"
+
+/* prefer not to use var because of issue in block scope and functional scope*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
