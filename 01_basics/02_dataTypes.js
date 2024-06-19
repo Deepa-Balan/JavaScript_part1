0213079d564061643731f38c04ea052ddc72cd74
Deepa-Balan <139548667+Deepa-Balan@users.noreply.github.com>
2024-06-19 17:25:47 +0000

    datatypes basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version.
+// alert(3 + 3) // We are using Node.js, not browser. So this will show error.
+
+console.log(3 + 3); 
+console.log("Deepa")
+
+let name = "Deepa" // string datatype
+let age = 5 // number datatype
+let isLoggedIn = false // boolean datatype
+let state; // undefined
+
+// the following are primitive datatypes
+// number size ranges to 2 to the power 53
+// if range exceeds then we use "bigint", mainly used in trading & stock market.
+// string => ""
+// boolean => true/false
+// null => standalone value, representation of empty value, it is an object
+// undefined => value is not defined
+// symbol => identify uniqueness
+
+
+// object
+
+console.log(typeof "Deepa")
+console.log(typeof age)
+console.log(typeof null) // object
