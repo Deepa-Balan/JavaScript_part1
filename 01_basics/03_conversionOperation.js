e5d7ea0546142f6298066640535aa726679ce4df
Deepa-Balan <139548667+Deepa-Balan@users.noreply.github.com>
2024-06-21 08:33:31 +0000

    Conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -40,6 +40,48 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
+// *********** Operations ************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// Basic operations are not discussed here
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+// Discuss only tricky things
+
+let str1 = "Hello"
+let str2 = " Deepa"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+// Never Do This
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+// Link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
 
 
 
